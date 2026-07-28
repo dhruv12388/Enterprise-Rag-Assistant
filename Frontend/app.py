10924022fa13804400ea98ababc19e64b8a8a33c
dhruv12388 <afinwaladhruv@gmail.com>
2026-07-28 14:23:08 +0530

    Update app.py

--- Frontend/app.py
+++ Frontend/app.py
@@ -13,7 +13,7 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-API_BASE = "  https://enterprise-rag-backend-xlc8.onrender.com"
+API_BASE = "https://enterprise-rag-backend-xlc8.onrender.com"
 QUERY_URL = f"{API_BASE}/documents/query"
  
 
